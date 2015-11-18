cf9225c0ada5f6ed50d8504d626a25eb41c1c52d
dahua777 <1220860737@qq.com>
2015-11-18 21:03:56 +0800

    LED
    第二次提交，更改了灯亮的时间间隔。

--- Blink.ino
+++ Blink.ino
@@ -25,14 +25,14 @@ void loop() {
   digitalWrite(13, HIGH);   // turn the LED on (HIGH is the voltage level)
   delay(10);              // wait for a second
   digitalWrite(13, LOW);    // turn the LED off by making the voltage LOW
-  delay(100);              // wait for a second
+  delay(50);              // wait for a second
   digitalWrite(13, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(100);              // wait for a second
+  delay(50);              // wait for a second
   digitalWrite(13, LOW);    // turn the LED off by making the voltage LOW
-  delay(100);              // wait for a second
+  delay(50);              // wait for a second
   digitalWrite(13, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(10);              // wait for a second
+  delay(50);              // wait for a second
   digitalWrite(13, LOW);    // turn the LED off by making the voltage LOW
-  delay(100);              // wait for a second
+  delay(50);              // wait for a second
   
 }
